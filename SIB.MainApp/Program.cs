51b8605dcb12a7ec98582eeeffd0abe57238b5ec
GOLEMWORKS <korotkih.vladimir.isr21.1@gmail.com>
2023-12-03 18:28:41 +0200

    Fix

--- SIB.MainApp/Program.cs
+++ SIB.MainApp/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Radzen;
+using SIB.MainApp.Data;
 
 namespace SIB.MainApp
 {
@@ -11,6 +12,9 @@ namespace SIB.MainApp
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddSingleton<Product>();
+            builder.Services.AddSingleton<ProductService>();
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddRadzenComponents();
