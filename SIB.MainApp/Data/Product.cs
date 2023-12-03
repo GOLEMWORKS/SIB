967b5b81129c4e8b954f1b646f1cede8b0798711
Аноним Анонимус <Alexsandr_the_third@mail.ru>
2023-12-03 17:50:58 +0200

    Добавления фильтра (без БД)(Демо)

---
+++ SIB.MainApp/Data/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace SIB.MainApp.Data
+{
+	public class Product
+	{
+		public int ProductID { get; set; }
+		public string? ProductName { get; set; }
+		public double Price { get; set; }
+		public DateTime PurchaseDate { get; set; }
+	}
+}
---
+++ SIB.MainApp/Data/ProductService.cs
@@ -0,0 +1,15 @@
+﻿namespace SIB.MainApp.Data
+{
+	public class ProductService
+	{
+		List<Product> products = new List<Product>() {
+			/* Здесь создаются объекты */
+			new Product(){ProductID = 1, ProductName="Computer",Price=50000, PurchaseDate=Convert.ToDateTime("03-Dec-2023")}
+		};
+
+		public async Task<List<Product>> ProductList()
+		{
+			return await Task.FromResult(products);
+		}
+	}
+}
