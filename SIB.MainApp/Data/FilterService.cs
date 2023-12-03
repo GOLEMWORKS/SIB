4a5f90c62727a9f621d77db334acd99774301142
Аноним Анонимус <Alexsandr_the_third@mail.ru>
2023-12-03 22:36:44 +0200

    Создание ссылок у TerminName (Недоработанная)

--- SIB.MainApp/Data/FilterService.cs
+++ SIB.MainApp/Data/FilterService.cs
@@ -4,7 +4,9 @@
 	{
 		List<Filter> Filters = new List<Filter>() {
 			/* Здесь создаются объекты */
-			new Filter(){FilterID = 1, TerminName="SIB",TerminNameT="СИБ", CreateDate=Convert.ToDateTime("03-Dec-2023")}
+			new Filter(){FilterID = 1, TerminName="SIB",TerminNameT="СИБ", CreateDate=Convert.ToDateTime("03-Dec-2023")},
+			new Filter(){FilterID = 2, TerminName="BIS",TerminNameT="БИС", CreateDate=Convert.ToDateTime("02-Dec-2023")},
+			new Filter(){FilterID = 3, TerminName="ISB",TerminNameT="ИСБ", CreateDate=Convert.ToDateTime("01-Dec-2023")}
 		};
 
 		public async Task<List<Filter>> FilterList()
