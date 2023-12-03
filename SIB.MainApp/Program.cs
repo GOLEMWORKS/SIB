cffeac3c1232dca575dea657fa3776c5842bd247
Аноним Анонимус <Alexsandr_the_third@mail.ru>
2023-12-03 19:51:49 +0200

    /

--- SIB.MainApp/Program.cs
+++ SIB.MainApp/Program.cs
@@ -13,7 +13,10 @@ namespace SIB.MainApp
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSingleton<Filter>();
+			builder.Services.AddSingleton<FilterService>();
+
+			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddRadzenComponents();
             await builder.Build().RunAsync();
         }
